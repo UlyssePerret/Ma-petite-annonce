d2a9a7488bc6c9886938c21fffcd04dc21527520
UlyssePerret <ulysseperret@orange.fr>
2021-11-30 13:12:19 +0100

    styles

--- ma-petite-annonce/src/App.js
+++ ma-petite-annonce/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/sass/App.scss';
+import Header from './components/Header';
+import CreateAnnonce from './components/CreateAnnonce';
+import Footer from './components/Footer';
+import Homepage from './components/Homepage';
+import Menu from './components/Menu';
+import OneAnnonce from './components/OneAnnonce';
+import PopupContact from './components/PopupContact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Menu />
+      <CreateAnnonce />
+
+      <Homepage />
+      <OneAnnonce />
+      <PopupContact />
+      <Footer />
     </div>
   );
 }
